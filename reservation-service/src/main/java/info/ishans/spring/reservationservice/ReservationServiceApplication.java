b531aeab09a4eecd42eb8cd9b18ff6cb1e32e02d
Ishan Somasiri <ishan@trustedcompany.com>
2018-01-01 23:47:55 +0800

    Reservation service gets registered in the Eureka service

--- reservation-service/src/main/java/info/ishans/spring/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/info/ishans/spring/reservationservice/ReservationServiceApplication.java
@@ -5,11 +5,13 @@ import info.ishans.spring.reservationservice.reservations.ReservationRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReservationServiceApplication {
 
 	@Bean
