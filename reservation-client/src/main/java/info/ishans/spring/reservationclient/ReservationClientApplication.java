6424d4942abd711cdcb2cdc63dc952bd52334c0b
Ishan Somasiri <ishan@trustedcompany.com>
2018-01-02 20:46:30 +0800

    Zuul client added

---
+++ reservation-client/src/main/java/info/ishans/spring/reservationclient/ReservationClientApplication.java
@@ -0,0 +1,16 @@
+package info.ishans.spring.reservationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ReservationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
+}
